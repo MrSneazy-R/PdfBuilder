bfcd4d1a4a5519959e38f3421cf75fcea5220b4d
MrSneazy-R <85078988+MrSneazy-R@users.noreply.github.com>
2026-08-18 14:14:20 +0200

    Use ZXingCpp for barcode generation

--- Imaging/BarcodePathBuilder.cs
+++ Imaging/BarcodePathBuilder.cs
@@ -2,8 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
-using ZXing;
-using ZXing.Common;
+using ZXingCpp;
 
 namespace PdfBuilder.Imaging
 {
@@ -22,28 +21,42 @@ namespace PdfBuilder.Imaging
             if (string.IsNullOrWhiteSpace(value))
                 throw new ArgumentException("Barcode value cannot be null or empty.", nameof(value));
 
-            var writer = new BarcodeWriterGeneric
+            var creator = new BarcodeCreator(ToFormat(kind));
+            using var barcode = creator.From(value);
+            if (!barcode.IsValid)
             {
-                Format = ToFormat(kind),
-                Options = BuildOptions(kind, quietZone)
+                throw new InvalidOperationException(
+                    $"Unable to encode barcode value '{value}': {barcode.ErrorMsg}");
+            }
+
+            using var writerOptions = new WriterOptions
+            {
+                Scale = 1,
+                AddHRT = false,
+                AddQuietZones = false
             };
+            using var image = barcode.ToImage(writerOptions);
+            var pixels = image.ToArray();
+            if (image.Format != ImageFormat.Lum || pixels.Length < image.Width * image.Height)
+            {
+                throw new InvalidOperationException(
+                    $"Barcode encoder returned an unsupported {image.Format} image.");
+            }
 
-            var matrix = writer.Encode(value);
-            if (matrix == null)
-                throw new InvalidOperationException($"Unable to encode barcode value '{value}'.");
-
-            float width = matrix.Width * moduleSize;
-            float height = Math.Max(1, matrix.Height) * moduleSize;
+            int matrixWidth = image.Width + (quietZone * 2);
+            int matrixHeight = image.Height + (quietZone * 2);
+            float width = matrixWidth * moduleSize;
+            float height = Math.Max(1, matrixHeight) * moduleSize;
 
             var pathBuilder = new StringBuilder();
             string fill = ToRgb(foregroundColor) ?? "0 0 0";
 
-            for (int y = 0; y < matrix.Height; y++)
+            for (int y = 0; y < image.Height; y++)
             {
                 int runStart = -1;
-                for (int x = 0; x <= matrix.Width; x++)
+                for (int x = 0; x <= image.Width; x++)
                 {
-                    bool isSet = x < matrix.Width && matrix[x, y];
+                    bool isSet = x < image.Width && pixels[(y * image.Width) + x] < 128;
                     if (isSet && runStart < 0)
                     {
                         runStart = x;
@@ -51,8 +64,8 @@ namespace PdfBuilder.Imaging
                     else if (!isSet && runStart >= 0)
                     {
                         int runLength = x - runStart;
-                        float rectX = runStart * moduleSize;
-                        float rectY = (matrix.Height - y - 1) * moduleSize;
+                        float rectX = (runStart + quietZone) * moduleSize;
+                        float rectY = (image.Height - y - 1 + quietZone) * moduleSize;
                         pathBuilder.AppendFormat(
                             Inv,
                             "{0} rg {1:0.###} {2:0.###} {3:0.###} {4:0.###} re f\n",
@@ -81,44 +94,14 @@ namespace PdfBuilder.Imaging
             return new BarcodeGeometry(width, height, pathBuilder.ToString(), backgroundCommand);
         }
 
-        private static EncodingOptions BuildOptions(Elements.BarcodeKind kind, int quietZone)
-        {
-            var opts = new EncodingOptions
-            {
-                Margin = quietZone,
-                PureBarcode = true
-            };
-
-            switch (kind)
-            {
-                case Elements.BarcodeKind.Code128:
-                case Elements.BarcodeKind.Code39:
-                    opts.Width = 1;
-                    opts.Height = 1;
-                    opts.Hints[EncodeHintType.CHARACTER_SET] = "UTF-8";
-                    break;
-                case Elements.BarcodeKind.Ean13:
-                    opts.Width = 1;
-                    opts.Height = 1;
-                    break;
-                case Elements.BarcodeKind.QrCode:
-                    opts.Width = 1;
-                    opts.Height = 1;
-                    opts.Hints[EncodeHintType.ERROR_CORRECTION] = ZXing.QrCode.Internal.ErrorCorrectionLevel.M;
-                    break;
-            }
-
-            return opts;
-        }
-
         private static BarcodeFormat ToFormat(Elements.BarcodeKind kind) =>
             kind switch
             {
-                Elements.BarcodeKind.Code128 => BarcodeFormat.CODE_128,
-                Elements.BarcodeKind.Code39 => BarcodeFormat.CODE_39,
-                Elements.BarcodeKind.Ean13 => BarcodeFormat.EAN_13,
-                Elements.BarcodeKind.QrCode => BarcodeFormat.QR_CODE,
-                _ => BarcodeFormat.QR_CODE
+                Elements.BarcodeKind.Code128 => BarcodeFormat.Code128,
+                Elements.BarcodeKind.Code39 => BarcodeFormat.Code39,
+                Elements.BarcodeKind.Ean13 => BarcodeFormat.EAN13,
+                Elements.BarcodeKind.QrCode => BarcodeFormat.QRCode,
+                _ => BarcodeFormat.QRCode
             };
 
         private static string? ToRgb(string? color)
